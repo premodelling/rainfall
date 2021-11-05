9122a6f5f0ae9abf3f96f57dd5eedc0789c88b0a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-05 21:40:59 +0000

    Update R/functions/HighestIndividualReadings.R

--- R/functions/HighestIndividualReadings.R
+++ R/functions/HighestIndividualReadings.R
@@ -1,15 +1,27 @@
-# Title     : HIR
-# Objective : HIR
+# Title     : Highest Individual Readings
+# Objective : The rainfall readings records that have the highest readings values
 # Created by: Think
 # Created on: 04/11/2021
 
 HighestIndividualReadings <- function (readings, stations) {
+  #' Highest Individual Readings
+  #'
+  #' Determines the rainfall readings records that have the highest readings values
+  #'
+  #' @param readings: The dataframe of rainfall readings, which must include the 'stationID' field, and the
+  #'                  'month', 'year', and 'rain' fields
+  #' @param stations: The dataframe of weather stations, which must include the 'stationID' & 'name' fields
+  #'
+  #'
+  #' @return A data frame that consists of the 5 reading records that have the highest rain
+  #'         quantity values,
 
-  expanded <- left_join(x = readings, y = stations, by = 'stationID', keep = FALSE)
-  expanded_ <- expanded[order(-expanded$rain), ]
-  expanded_ %>%
-    tibble()
+  joined <- left_join(x = readings[, c('stationID', 'month', 'year', 'rain')],
+                      y = stations[, c('stationID', 'name')],
+                      by = 'stationID',
+                      keep = FALSE)
+  frame <- joined[order(-joined$rain), ]
 
-  return(head(expanded_, n = 5))
+  return(head(frame, n = 5))
 
 }
