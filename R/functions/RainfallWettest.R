cbb48258b0634f14ab68e206761abc8cc62efdcc
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-06 00:12:01 +0000

    Update R/functions/RainfallWettest.R

--- R/functions/RainfallWettest.R
+++ R/functions/RainfallWettest.R
@@ -3,14 +3,16 @@
 # Created by: Think
 # Created on: 02/11/2021
 
-RainfallWettest <- function (data) {
+#'
+#'
+#' The mean rainfall by month [across all years]
+#'
+RainfallWettest <- function (averages) {
 
   # Wettest month
-  # Instead of arrange ... wettest[order(wettest$month_code), ]
-  wettest <- data %>%
-    group_by(month, month_code) %>%
-    summarise(mean = mean(mean_rainfall_month), .groups = 'drop') %>%
-    arrange(month_code)
+  wettest <- averages %>%
+    group_by(month) %>%
+    summarise(mean = mean(mean_rainfall_month), .groups = 'drop')
 
   return(wettest)
 
