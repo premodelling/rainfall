5b22308e481d3b8d412c6bf32968bf5d6320d2bc
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-06 00:35:23 +0000

    Update R/functions/RainfallAveragesReduce.R

--- R/functions/RainfallAveragesReduce.R
+++ R/functions/RainfallAveragesReduce.R
@@ -1,13 +1,18 @@
-# Title     : The Wettest Months
-# Objective : Determine the wettest month via average rainfall per month data (covering a few years)
+# Title     : Rainfall Averages Reduce
+# Objective : Determine the average rainfall per month [across all years]
 # Created by: Think
 # Created on: 02/11/2021
 
-#'
+#' Rainfall Averages Reduce
 #'
 #' The mean rainfall by month [across all years]
 #'
-RainfallWettest <- function (averages) {
+#' @param averages: A dataframe wherein each record is the mean rainfall of a month of a year
+#'
+#' @return A dataframe of 12 records.  Each record includes the average amount of rainfall
+#'         a month has had [across all years]
+#'
+RainfallAveragesReduce <- function (averages) {
 
   # Wettest month
   wettest <- averages %>%
