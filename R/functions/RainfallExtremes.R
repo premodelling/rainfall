1badce85fea472b0f99ae97c0eb25f54c7564064
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-06 00:15:00 +0000

    Update R/functions/RainfallExtremes.R

--- R/functions/RainfallExtremes.R
+++ R/functions/RainfallExtremes.R
@@ -3,9 +3,15 @@
 # Created by: Think
 # Created on: 04/11/2021
 
-RainfallExtremes <- function (data) {
+#' Rainfall Extremes
+#'
+#' Inspecting extreme rain quantities per station VIA total rainfall per station per year
+#'
+#' @param readings: The dataframe of rainfall readings
+#'
+RainfallExtremes <- function (readings) {
 
-  extremes <- data %>%
+  extremes <- readings %>%
     group_by(stationID, year) %>%
     summarise(total = sum(rain), .groups = 'drop')
 
