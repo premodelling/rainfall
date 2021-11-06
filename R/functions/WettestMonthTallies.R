366e92e40e150fa628fac2de8820d2349eec9b81
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-06 00:15:16 +0000

    Update R/functions/WettestMonthTallies.R

--- R/functions/WettestMonthTallies.R
+++ R/functions/WettestMonthTallies.R
@@ -1,23 +1,38 @@
-# Title     : The wettest months
-# Objective : The wettest months
+# Title     : Wettest Month tallies
+# Objective : The annual 'month with the most rainfall crown'
 # Created by: Think
 # Created on: 04/11/2021
 
+#' Wettest Month Tallies
+#'
+#' Determine the number of times each month takes the annual 'month with the most rainfall crown'
+#'
+#' @param readings: The dataframe of rainfall readings, which must include 'year', 'month', and 'rain'
+#'
+#' Future: Try the calculation below via the function split()
+#'
 WettestMonthTallies <- function (readings) {
 
+  # Calculating the total amount of rain per month each year
   baseline <- readings %>%
     group_by(year, month) %>%
-    summarise(total_rainfall = sum(rain), .groups = 'drop')
+    summarise(total = sum(rain), .groups = 'drop')
 
-  ordered_baseline <- baseline[with(baseline, order(year, -total_rainfall)), ]
+  # Ordering 'baseline' by (a) year, and (b) decreasing total rainfall
+  rearranged <- baseline[with(baseline, order(year, -total)), ]
 
-  indices <- !duplicated(ordered_baseline$year)
-  wettest_months_per_year <- ordered_baseline[indices, ]
-
-  frequencies <- wettest_months_per_year %>%
+  # Using !duplicated() to mark the first record of each year.  Each first record
+  # is the record/month that has the highest rainfall during the year in question - due
+  # to the ordering statement above
+  indices <- !duplicated(rearranged$year)
+  records <- rearranged[indices, ]
+  
+  # How many times does each month occur in 'records'?
+  frequencies <- records %>%
     group_by(month) %>%
     summarise(N = n())
 
+  # return
   return(frequencies)
 
 }
