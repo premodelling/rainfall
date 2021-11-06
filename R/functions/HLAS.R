4197fd987342fad61f790766fce56f9be75bfa25
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-06 00:11:47 +0000

    Update R/functions/HLAS.R

--- R/functions/HLAS.R
+++ R/functions/HLAS.R
@@ -1,17 +1,35 @@
 # Title     : Highest & Lowest Annual Stations
-# Objective : Highest & Lowest Annual Stations
+# Objective : The records that have the highest & lowest annual rainfall records
 # Created by: Think
 # Created on: 04/11/2021
 
+#' Highest & Lowest Annual Station Records
+#'
+#' Determines the set of records that are the five highest, and five lowest, annual rainfall records in the
+#' extremes table
+#'
+#' @param extremes: The dataframe of annual rainfall records per station, which must include 
+#'                  the 'stationID' field, and the 'year' and 'total' fields
+#' @param stations: The dataframe of weather stations, which must include the 'stationID' & 'name' fields
+#'
+#' @return A named list of 2 dataframes: (1) 'highest' = highest (2) 'lowest' = lowest
+#'
 HLAS <- function (extremes, stations) {
 
-  extremes_ <- left_join(x = extremes, y = stations[, c('stationID', 'name')], by = 'stationID', keep = FALSE)
-  extremes_ <- extremes_[order(-extremes_$total), ]
-  highest_set <- head(extremes_, n = 5)
+  # Joining the extremes & stations tables
+  joined <- left_join(x = extremes, y = stations[, c('stationID', 'name')], by = 'stationID', keep = FALSE)
 
-  lowest_set <- tail(extremes_, n = 5) %>%
+  # Ordering the resulting table in descending order w.r.t. total, i.e., total rainfall
+  frame <- joined[order(-joined$total), ]
+
+  # The five records that the highest total rainfall values
+  highest <- head(frame, n = 5)
+
+  # The five records that the lowest total rainfall values; in ascending order
+  lowest <- tail(frame, n = 5) %>%
     arrange(total)
 
-  return(list('highest' = highest_set, 'lowest' = lowest_set))
+  # return
+  return(list('highest' = highest, 'lowest' = lowest))
 
 }
